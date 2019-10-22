47f28073090675b6ad6f29427d75fe4f15e9ce0d
Femi Abdul <femi.abdul67@gmail.com>
2019-10-22 12:31:56 +0300

    added authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations'
+  }
+  devise_scope :users do
+    get 'signup', to: 'users/registrations#new'
+    get 'signin', to: 'users/sessions#new'
+    delete 'signout', to: 'users/sessions#destroy'
+  end
+
   resources :users
   root 'pages#home'
 
-  get '/register', to: 'users#new' 
+  get '/register', to: 'users#new'
+end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191020195700) do
+ActiveRecord::Schema.define(version: 20191022092126) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20191020195700) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "role"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
